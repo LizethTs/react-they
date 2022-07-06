e1082313d44e99e8080e9665b208cd9a388fa47b
Lizeth Tancara <liztancarasirpa@gmail.com>
2022-07-05 22:54:06 -0300

    agregando ItemCount en el componente de ItemListConainer.js

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({greeting}) => {
-    return <div>{greeting}</div>;
+    return (
+        <div>
+            {greeting}
+            <ItemCount 
+                stock={5} 
+                initial={1} 
+                onAdd={(n) => alert(`Agregados ${n} productos`)} 
+            />
+        </div>
+    );
 };
 
 export default ItemListContainer;
