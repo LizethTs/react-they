46a3af5a79ba6f0cb7b500fcb26b61847c55dfd5
Lizeth Tancara <liztancarasirpa@gmail.com>
2022-07-11 18:34:43 -0300

    agregando el componente Item.js y Item.css

---
+++ src/components/Item.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Item.css";
+
+const Item = ({ id, title, description, price, image} ) => {
+    return (
+        <div className="itemContainer">
+            <div className="imageContainer">
+                <img src={image} alt={title} className="imageStyle"/>
+            </div>
+            <h3>{title}</h3>
+            <p>{description.substr(0,20)}</p>
+            <p>{price}</p>
+        </div>      
+    );
+};
+ 
+export default Item;
