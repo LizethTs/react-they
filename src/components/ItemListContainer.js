b32e4bdd5bb41e69705f0a734d2c5b02c8ab7b90
Lizeth Tancara <liztancarasirpa@gmail.com>
2022-07-11 18:44:50 -0300

    agregando el componente de ItemList.js en ItemListContainer.js

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import ItemCount from './ItemCount';
+import React, { useEffect, useState } from "react";
+/*import ItemCount from "./ItemCount";*/
+import ItemList from "./ItemList";
+import products from "../products.json";
 
-const ItemListContainer = ({greeting}) => {
-    return (
-        <div>
-            {greeting}
-            <ItemCount 
-                stock={5} 
-                initial={1} 
-                onAdd={(n) => alert(`Agregados ${n} productos`)} 
-            />
-        </div>
-    );
+const ItemListContainer = ({ greeting }) => {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(true);
+    }, 2000);
+  });
+
+  // if(!loading){
+  //     return "loading..."
+  // }
+
+  return (
+    <div style={{ backgroundColor: "#EBEBEB", padding: "0 20px" }}>
+     <h2 style={{fontSize: "36px", fontWeight: "400", padding: "30px" }}>{greeting}</h2>
+      {/* <ItemCount
+        stock={5}
+        initial={1}
+        onAdd={(n) => alert(`Agregados ${n} productos`)}
+      /> */}
+      {!loading ? <p>loading...</p> : <ItemList items={products} />}
+    </div>
+  );
 };
 
 export default ItemListContainer;
